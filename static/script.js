1e88e32fa48eaee82cf4eff3febfa6c16aa0a48d
jagmeet kaur <meetkaur@jagmeets-MacBook-Pro.local>
2026-03-14 20:39:15 +0530

    Improve UI with dark mode and button animation

--- static/script.js
+++ static/script.js
@@ -1,4 +1,6 @@
+const clickSound = new Audio("https://assets.mixkit.co/active_storage/sfx/2571/2571-preview.mp3");
 function getSuggestion() {
+    clickSound.play();
     const goal = document.getElementById("goal").value;
     const time = document.getElementById("time").value;
 
